6beca3ad2ac71e22848d698f34154baecd1454aa
Madhavi <madhavikh@cybage.com>
2015-08-13 18:04:46 +0530

    2nd commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,36 @@
+/*running
+module.exports = function(grunt) {
+  var nightwatch = require('nightwatch');
+  nightwatch.initGrunt(grunt);
+  grunt.initConfig({
+  nightwatch: {
+    options: {
+      cwd: './'
+    },
+
+    'default' : {},
+
+    browserstack: {
+      argv: {
+        env: 'browserstack'
+      },
+      settings: {
+        silent: true
+      }
+    },
+
+    'all' : {
+      argv: {
+        env: 'default,browserstack'
+      }
+    },
+  }
+});
+
+ grunt.registerTask('default', ['nightwatch']);
+
+};*/
+
 module.exports = function(grunt) {
 
   // Project configuration.
@@ -46,12 +79,12 @@ var nightwatch = require('nightwatch');
 
   });
 
-  // Actually load this plugin's task(s).
- // grunt.loadTasks('tasks');
+  //Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
   grunt.loadNpmTasks('grunt-nightwatch-report');
   grunt.loadNpmTasks('grunt-contrib-clean');
     
-  grunt.registerTask('gen-test', ['clean','nightwatch', 'nightwatch_report']);
+ grunt.registerTask('gen-test', ['clean','nightwatch', 'nightwatch_report']);
     
   grunt.registerTask('default', ['gen-test']);
 
