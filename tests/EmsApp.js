1f3020a897ee6b93c74ebb5233030fd48dcaf303
madhavikhatawakar <madhavikh@cybage.com>
2015-08-14 10:52:15 +0530

    saved123

--- tests/EmsApp.js
+++ tests/EmsApp.js
@@ -1,4 +1,4 @@
-module.exports = {
+/*module.exports = {
   'Demo test Ems App' : function (client) {
     client
       .url('http://localhost:5024/EmployeeApplicationSprint4-1.0-SNAPSHOT')
@@ -13,4 +13,20 @@ module.exports = {
   }
     
    
+};*/
+
+  'Demo test Google' : function (client) {
+    client
+      .url('http://www.google.com')
+      .waitForElementVisible('body', 1000)
+      .assert.title('Google')
+      .assert.visible('input[type=text]')
+      .setValue('input[type=text]', 'rembrandt van rijn')
+      .waitForElementVisible('button[name=btnG]', 1000)
+      .click('button[name=btnG]')
+      .pause(1000)
+      .assert.containsText('ol#rso li:first-child',
+        'Rembrandt - Wikipedia')
+      .end();
+  }
 };
